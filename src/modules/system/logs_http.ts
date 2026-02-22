6774b1bb1e9d6b99dacb0a7193f1caae76a206d2
Srishti-04 <srishtijaiswal2823@gmail.com>
2026-02-22 14:25:47 +0100

    logging page

--- src/modules/system/logs_http.ts
+++ src/modules/system/logs_http.ts
@@ -4,19 +4,13 @@ export class LogsHttp {
   constructor(private logsRepository: LogsRepository) {}
 
   async getLogsPageVariables(request: any, response: any): Promise<any> {
-    let excludeLevels: string[] = request.query.exclude_levels || [];
+    // Check for query params (support both exclude_levels and exclude_levels[])
+    let excludeLevels: string[] = request.query.exclude_levels || request.query['exclude_levels[]'] || ['debug'];
+
     // Ensure it's always an array (Express sends single value as string)
     if (typeof excludeLevels === 'string') {
       excludeLevels = [excludeLevels];
     }
-
-    if (excludeLevels.length === 0 && !('filters' in request.cookies)) {
-      excludeLevels = ['debug'];
-    }
-
-    response.cookie('filters', excludeLevels, {
-      maxAge: 60 * 60 * 24 * 30 * 1000
-    });
 
     return {
       logs: await this.logsRepository.getLatestLogs(excludeLevels),
